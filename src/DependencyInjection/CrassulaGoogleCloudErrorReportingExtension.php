44ef337b871b5d1f6e42603cf1313cf54291d707
luxemate <luxemate1@gmail.com>
2018-10-23 14:29:35 +0300

    Add copyright headers to files

--- src/DependencyInjection/CrassulaGoogleCloudErrorReportingExtension.php
+++ src/DependencyInjection/CrassulaGoogleCloudErrorReportingExtension.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the crassula/google-cloud-error-reporting-bundle package.
+ *
+ * (c) Crassula <hello@crassula.io>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
 namespace Crassula\Bundle\GoogleCloudErrorReportingBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
